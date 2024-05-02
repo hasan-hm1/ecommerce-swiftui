e4861e3879f014514c77f273f25dececfbc72177
Hasan Mohammed <hasanmohammed.hm1@gmail.com>
2024-05-02 10:57:19 +0300

    Initial Commit

---
+++ MyShop/MyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShopApp.swift
+//  MyShop
+//
+//  Created by Hasan on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
